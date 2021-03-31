e040f11ee17998cfbc24fbe6091df825822ca34f
Dave Holst <daveholst@gmail.com>
2021-03-31 21:34:51 +0800

    rebuilt local data -> js object builder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,15 @@
-// class object requires momentJS
-// takes args as 24-hr. syntax -> 'hh:mm' || timeSlot in minutes (int-type)
-
-
-
-
-//TESTING
-
-//Check for Local
-if(window.localStorage.getItem('myDay'))
-
+// assumes that the local storage was generated with same args as below. Could Probably validate this?
 const myDay = new DayPlanner('09:00', '18:00', 60);
 
-myDay.buildObject();
-myDay.tableBuilder()
-console.log(myDay);
+//check local object exists, if not. build!
+if (localStorage.getItem('myDay')) {
+  // read in the data from local storage and build data object
+  myDay.buildFromLocal();
+  // build the table in the DOM
+  myDay.tableBuilder();
+} else {
+  // build new data object
+  myDay.buildObject();
+  // build table to
+  myDay.tableBuilder();
+}
--- assets/js/dayPlanner.js
+++ assets/js/dayPlanner.js
@@ -11,16 +11,8 @@ class DayPlanner {
   }
   //build the dayplanner storage object.
   buildObject() {
-    //time calc function -
-    const timeCalc = (modifier = 0) => {
-      // create a copy of the formatted start time
-      let copy = this.startTime.format();
-      // calculate new time and assign to copy
-      copy = (() => moment(copy)
-        .add(this.timeSlotDuration * (i + modifier), 'm')
-        .subtract(modifier, 'seconds'))();
-      return copy;
-    }
+    //time calc function - Moved to seperate method
+
     // build array of objects (based on input arguments [start,end, interval])
     for (var i = 0; i < this.totalTimeSlots; i++) {
       // add blank object
@@ -28,8 +20,8 @@ class DayPlanner {
       // populate object
       this.data[i] = {
         description: '',
-        slotStart: timeCalc(),
-        slotEnd: timeCalc(1),
+        slotStart: this.timeCalc(0,i),
+        slotEnd: this.timeCalc(1,i),
         slotDuration: this.timeSlotDuration,
         get viewState() {
           let timeDiff = (() => moment().diff(this.slotStart, 'm'))()
@@ -41,23 +33,51 @@ class DayPlanner {
     }
   }
 
-  //write to LS
+  //write to LS -
   writeToLocal() {
     window.localStorage.removeItem('myDay');
     const json = JSON.stringify(this);
     window.localStorage.setItem('myDay', json);
   }
   //read from LS --
-  readFromLocal() {
-    const parsed = JSON.parse(window.localStorage.getItem('myDay'));
-    return parsed;
+  buildFromLocal() {
+    //grab data
+    const localParsed = JSON.parse(window.localStorage.getItem('myDay'));
+    // console.log(localParsed.data[0].description);
+    // turn props back into functions/objects.
+    this.dateCreated = moment(localParsed.dateCreated);
+    this.startTime = moment(localParsed.startTime);
+    this.endTime  = moment(localParsed.endTime)
+    this.timeSlotDuration = localParsed.timeSlotDuration;
+    this.totalTimeSlots = localParsed.totalTimeSlots;
+
+    //TODO: some sort of loop?
+    for (var i = 0; i < this.totalTimeSlots; i++) {
+      // add blank object
+      this.data.push({})
+      // populate object
+      this.data[i] = {
+        description: localParsed.data[i].description,
+        slotStart: moment(localParsed.data[i].slotStart),
+        slotEnd: moment(localParsed.data[i].slotStart),
+        slotDuration: this.timeSlotDuration,
+        get viewState() {
+          let timeDiff = (() => moment().diff(this.slotStart, 'm'))()
+          if (timeDiff < 0) return 'future';
+          else if (timeDiff >= 0 && timeDiff < 60) return 'present';
+          else return 'past';
+        }
+      }
+    }
+
+
+    // return parsed;
+    console.log(this);
   }
   //table builder
   tableBuilder() {
     //jQuery Selectors
-    const tableBody = $('#table-body')
-    //create table from object.data
-    const tableRawData = this.data;
+    const tableBody = $('#table-body');
     //table builder iterator
     for (let i = 0; i < this.data.length; i++){
       // create and append <tr> with class based off state (past,present,future )
@@ -72,12 +92,12 @@ class DayPlanner {
       newRow.append(newHourCell);
       //add description cell
       let newDescriptionCell = $('<td>')
-        .text(this.data[i].description)
-        .addClass(`description ${this.data[i].viewState}`);
+      .addClass(`description ${this.data[i].viewState}`);
       newRow.append(newDescriptionCell);
       //add text area
       let newTextArea = $('<textarea>')
-        .attr('id', `text-area-${i}`);
+        .attr('id', `text-area-${i}`)
+        .text(this.data[i].description);
       newDescriptionCell.append(newTextArea);
       //add save button/icon
       let newSaveButton = $('<td>')
@@ -98,6 +118,15 @@ class DayPlanner {
       };
     };
 
+    timeCalc(modifier = 0,i) {
+      // create a copy of the formatted start time
+      let copy = this.startTime.format();
+      // calculate new time and assign to copy
+      copy = (() => moment(copy)
+        .add(this.timeSlotDuration * (i + modifier), 'm')
+        .subtract(modifier, 'seconds'))();
+      return copy;
+    }
   }
 
 
