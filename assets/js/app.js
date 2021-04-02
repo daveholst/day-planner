19df1cf59a0b207fb3a223b9ed05b86ced25f473
Dave Holst <daveholst@gmail.com>
2021-04-02 16:41:02 +0800

    adjusted media-q and added day checker --> rebuild if yesterdays data

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,24 @@ const myDay = new DayPlanner('09:00', '18:00', 60);
 
 //check local object exists, if not. build!
 if (localStorage.getItem('myDay')) {
+  // TODO: check if it is for today? if not, REBUILD!!!
   // read in the data from local storage and build data object
   myDay.buildFromLocal();
   // build the table in the DOM
   myDay.tableBuilder();
-} else {
-  // build new data object
-  myDay.buildObject();
-  // build table to
-  myDay.tableBuilder();
-}
+  // check if data is from yesterday? if so rebuild!
+  if (moment().diff(myDay.dateCreated, 'days') > 1) {
+    // rebuild new object
+    myDay.buildObject();
+    // rebuild table
+    myDay.tableBuilder();
+  }
+  } else {
+    // build new data object
+    myDay.buildObject();
+    // build table to
+    myDay.tableBuilder();
+  }
+
+// TESTING -- log out days different
+console.log('days diff from created',moment().diff(myDay.dateCreated, 'days'));
