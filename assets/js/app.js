3565bb48a8cc89e3570484a400044411483d2e65
Dave Holst <daveholst@gmail.com>
2021-04-05 08:40:33 +0800

    changed table building logic if previosu days data exists

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,16 +10,15 @@ let myDay = new DayPlanner('09:00', '18:00', 60);
 if (localStorage.getItem('myDay')) {
   // read in the data from local storage and build data object
   myDay.buildFromLocal();
-  // build the  table in the DOM
-  myDay.tableBuilder();
   //check if data is from yesterday? if so rebuild!
   if (moment().isSame(myDay.dateCreated, 'days') === false) {
     // rebuild new object
     myDay = new DayPlanner('09:00', '18:00', 60);
     myDay.buildObject();
-    myDay.tableBuilder();
     myDay.writeToLocal();
   }
+  // build the  table in the DOM
+  myDay.tableBuilder();
   } else {
     // build new data object & table
     myDay.buildObject();
